637588dfd1cb6c63addb310e03e38b2de29f3cab
Fernand <fernandajavon@hotmail.com>
2020-01-12 06:15:11 -0500

    Update Soumission.js

--- src/components/Soumission.js
+++ src/components/Soumission.js
@@ -9,31 +9,73 @@ const ages = ['Choisir', '15 à 20', '21 à 25', '26 à 30', '31 à 35', '36 à 
 const experiences = ['Choisir', '- 24 Mois', '2 à 5 ans', '6 à 9 ans', '10 ans +'];
 const sinistres = ['Choisir', '0', '1', '2', '3+'];
 
-const annees = ['Année', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009'];
-const marques = ['Marque', 'Audi', 'BMW', 'Chevrolet', 'Ford', 'Honda', 'Hyundai', 'Mazda'];
-const modeles = ['Modèle', 'aaaa', 'bbbb', 'cccc', 'dddd', 'eeee'];
-const kmas = ['Km par année', 'moins de 10 000 km', '10 001 à 20 000 km', '20 001 à 35 000 km', 'plus de 35 000 km']
+const annees = ['Choisir', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009'];
+const marques = ['Choisir', 'Chevrolet', 'Ford', 'Honda', 'Hyundai', 'Mazda', 'Nissan'];
+const kmas = ['Choisir', 'moins de 10 000 km', '10 001 à 20 000 km', '20 001 à 35 000 km', 'plus de 35 000 km']
 
+const chevrolet = ['Choisir', 'Blazer', 'Camaro', 'Cruze', 'Equinox', 'Silverado'];
+const ford =  ['Choisir', 'EcoSport', 'Escape', 'Explorer', 'F-150', 'Fusion', 'Mustang'];
+const honda = ['Choisir', 'Accord', 'Civic', 'Civic Coupe', 'CR-V', 'HR-V'];
+const hyundai = ['Choisir', 'Elantra', 'Elantra GT', 'Kona', 'Kona Electric', 'Palisade', 'Santa Fe', 'Sonata', 'Tucson'];
+const mazda = ['Choisir', 'CX-3', 'CX-5', 'CX-9', 'Mazda3', 'Mazda6'];
+const nissan = ['Choisir', 'Altima', 'Kicks', 'Micra', 'Murano', 'Quashqai', 'Rogue', 'Versa'];
+
+const p = 'Prénom';
 
 class Soumission extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       InfoC, InfoV,
+      modeles: [],
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleMarques = this.handleMarques.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
-  handleChange(event) {
-    this.setState({ prenom: event.target.value });
+  handleMarques(event) {
+    // if (event.target.value === 'Chevrolet') {
+    //   this.setState({modeles: chevrolet})
+    // }
+    switch (event.target.value) {
+      case 'Chevrolet':
+        this.setState({modeles: chevrolet})
+        break;
+
+      case 'Ford':
+        this.setState({modeles: ford})
+        break;
+
+      case 'Honda':
+        this.setState({modeles: honda})
+        break;
+
+      case 'Hyundai':
+        this.setState({modeles: hyundai})
+        break;
+
+      case 'Mazda':
+        this.setState({modeles: mazda})
+        break;
+
+      case 'Nissan':
+        this.setState({modeles: nissan})
+        break;
+
+      default:
+        break;
+    }
+    this.setState({ marque: event.target.value });
   }
+
 
   handleSubmit(event) {
-    alert('Genre : ' + this.state.genre + '   Prénom : ' + this.state.prenom + '    Nom : ' + this.state.nom + '\nÂge : ' + this.state.age + '\nExpérience : ' + this.state.experience + '\nSinitres : ' + this.state.sinistres + '\nTéléphone : ' + this.state.tel + '\nCode Postal : ' + this.state.cp + '\nAnnée : ' + this.state.annee + '\nMarque : ' + this.state.marque + '\nModèle : ' + this.state.modele + '\nKm/Année : ' + this.state.kma);
+    alert('Genre : ' + this.state.genre + '   Prénom : ' + this.state.prenom + '    Nom : ' + this.state.nom + '\nÂge : ' + this.state.age + '\nExpérience : ' + this.state.experience + '\nSinitres : ' + this.state.sinistre + '\nTéléphone : ' + this.state.tel + '\nCode Postal : ' + this.state.cp + '\nMarque : ' + this.state.marque + '\nModèle : ' + this.state.modele + '\nAnnée : ' + this.state.annee  + '\nKm/Année : ' + this.state.kma);
     event.preventDefault();
   }
+
 
   render() {
     return (
@@ -44,15 +86,13 @@ class Soumission extends React.Component {
 
           <form className="" onSubmit={this.handleSubmit}>
           
-            <div className="row justify-content-around">
               
-              <div className="col-8 border rounded px-4 mt-2">
+              <div className="border rounded px-4 mt-2">
                 <h3>Conducteur</h3>
 
                 <div className="form-group row justify-content-start"> 
 
-                  <div className="col-2">
-                  <label>Civilité</label>
+                  <label className="col-2">Civilité
                   <select className="form-control" onChange={event => { this.setState({ genre: event.target.value }) }}>
                     {genres.map(genre => (
                       <option key={genre} value={genre}>
@@ -60,25 +100,21 @@ class Soumission extends React.Component {
                       </option>
                     ))}
                   </select>
-                  </div>
+                  </label>
 
-                  <div className="col-5">
-                  <label>Prénom</label>
-                  <input type="text" placeholder="Prénom" className="form-control" onChange={event => { this.setState({ prenom: event.target.value }) }} />
-                  </div>
+                  <label className="col-5">Prénom
+                    <input type="text" placeholder={p} className="form-control" onChange={event => { this.setState({ prenom: event.target.value }) }} />
+                  </label>
 
-                  <div className="col-5">
-                  <label>Nom</label>
-                  <input type="text" placeholder="Nom" className="form-control" onChange={event => { this.setState({ nom: event.target.value }) }} />
-                  </div>
+                  <label className="col-5">Nom
+                    <input type="text" placeholder="Nom" className="form-control" onChange={event => { this.setState({ nom: event.target.value }) }} />
+                  </label>
                 </div>
 
-                {/* <div className="form-group. row. justify-content-start."> */}
 
                   <div className="form-group row justify-content-start">
 
-                    <div className="col-4">
-                      <label>Tranche d'Âge</label>
+                    <label className="col">Tranche d'Âge
                       <select className="form-control" onChange={event => { this.setState({ age: event.target.value }) }}>
                         {ages.map(age => (
                           <option key={age} value={age}>
@@ -86,10 +122,9 @@ class Soumission extends React.Component {
                           </option>
                         ))}
                       </select>
-                    </div>
+                    </label>
 
-                    <div className="col-4">
-                      <label>Experience</label>
+                    <label className="col">Experience
                       <select className="form-control" onChange={event => { this.setState({ experience: event.target.value }) }}>
                         {experiences.map(experience => (
                           <option key={experience} value={experience}>
@@ -97,33 +132,29 @@ class Soumission extends React.Component {
                           </option>
                         ))}
                       </select>
-                    </div>
+                    </label>
 
-                    <div className="col-4">
-                      <label>Sinistres</label>
-                      <select className="form-control" onChange={event => { this.setState({ experience: event.target.value }) }}>
+                    <label className="col">Sinistres
+                      <select className="form-control" onChange={event => { this.setState({ sinistre: event.target.value }) }}>
                         {sinistres.map(sinistre => (
                           <option key={sinistre} value={sinistre}>
                             {sinistre}
                           </option>
                         ))}
                       </select>
-                    </div>
+                    </label>
 
                   </div>
-                {/* </div> */}
                     
                 <div className="form-group row justify-content-start">
 
-                  <div className="col-4">
-                  <label>Code Postal</label>
-                  <input type="text" placeholder="A1B C2D" className="form-control" onChange={event => { this.setState({ cp: event.target.value }) }} />
-                  </div>
+                  <label className="col-4">Code Postal
+                    <input type="text" placeholder="A1B C2D" className="form-control" onChange={event => { this.setState({ cp: event.target.value }) }} />
+                  </label>
 
-                  <div className="col-4">
-                  <label>Telephone</label>
-                  <input type="phone" placeholder="123-456-7890" className="form-control" onChange={event => { this.setState({ tel: event.target.value }) }} />
-                  </div>
+                  <label className="col-4">Telephone
+                    <input type="phone" placeholder="123-456-7890" className="form-control" onChange={event => { this.setState({ tel: event.target.value }) }} />
+                  </label>
 
                 </div>
 
@@ -140,55 +171,61 @@ class Soumission extends React.Component {
 
               </div>
 
-              <div className="col-4 border rounded px-5 mt-2">
+              <div className="col-4. border rounded px-4 mt-2">
                 <h3>Véhicule</h3>
-                {/* Annee */}
-                <div class="form-group">
-                  <select className="form-control" onChange={event => { this.setState({ annee: event.target.value }) }}>
-                    {annees.map(annee => (
-                      <option key={annee} value={annee}>
-                        {annee}
-                      </option>
-                    ))}
-                  </select>
+
+
+                <div className="form-group row justify-content-start">
+
+                  <label className="col-5">Marque
+                    <select className="form-control" onChange={this.handleMarques}>
+                      {marques.map(marque => (
+                        <option key={marque} value={marque}>
+                          {marque}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+
+                  <label className="col-5">Modèle
+                    <select className="form-control" onChange={event => { this.setState({ modele: event.target.value }) }}>
+                      {this.state.modeles.map(modele => (
+                        <option key={modele} value={modele}>
+                          {modele}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+
                 </div>
 
-                {/* Tranche d'Age */}
-                <div class="form-group" onChange={event => { this.setState({ marque: event.target.value }) }}>
-                  <select className="form-control">
-                    {marques.map(marque => (
-                      <option key={marque} value={marque}>
-                        {marque}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                <div className="form-group row justify-content-start">
 
-                {/* Experience de conduite */}
-                <div class="form-group">
-                  <select className="form-control" onChange={event => { this.setState({ modele: event.target.value }) }}>
-                    {modeles.map(modele => (
-                      <option key={modele} value={modele}>
-                        {modele}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                  <label className="col-5">Année
+                    <select className="form-control" onChange={event => { this.setState({ annee: event.target.value }) }}>
+                      {annees.map(annee => (
+                        <option key={annee} value={annee}>
+                          {annee}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
 
-                {/* Kilomètres par années */}
-                <div class="form-group">
-                  <select className="col-5. form-control" onChange={event => { this.setState({ kma: event.target.value }) }}>
-                    {kmas.map(kma => (
-                      <option key={kma} value={kma}>
-                        {kma}
-                      </option>
-                    ))}
-                  </select>
+                  <label className="col-5">Km par Année
+                    <select className="form-control" onChange={event => { this.setState({ kma: event.target.value }) }}>
+                      {kmas.map(kma => (
+                        <option key={kma} value={kma}>
+                          {kma}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
                 </div>
 
               </div>
             
-            </div>
+            {/* </div> */}
+            
               <div className="row justify-content-center mt-5">
                 {/* <button class="btn btn-secondary mx-2" type="reset">Effacer</button> */}
 
