a25dfc975fe7cc20412af9c25db346b4d6c622ac
Ayi05 <fernandajavon@hotmail.com>
2020-01-29 23:24:24 -0500

    Updates

---
+++ precache-manifest.2fc1a670044d20535e964b2b7eef781d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa451faff19e163201c36e7edd8bff3",
+    "url": "/demosoumission/index.html"
+  },
+  {
+    "revision": "c77634c10e590f01c59c",
+    "url": "/demosoumission/static/css/main.8060dbe6.chunk.css"
+  },
+  {
+    "revision": "c2a66498f1286435d227",
+    "url": "/demosoumission/static/js/2.d2d9240c.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/demosoumission/static/js/2.d2d9240c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c77634c10e590f01c59c",
+    "url": "/demosoumission/static/js/main.39e88bd5.chunk.js"
+  },
+  {
+    "revision": "da8059bdb6b631496c46",
+    "url": "/demosoumission/static/js/runtime-main.3df925df.js"
+  }
+]);
